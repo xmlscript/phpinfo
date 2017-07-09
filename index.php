3f46cd8989decaa9b842b0af8e65833b51f5b14d
xmlscript <xmlscript@gmail.com>
2017-07-09 23:43:42 +0800

    dev:

--- index.php
+++ index.php
@@ -1,3 +1,134 @@
+<?php
+$debug = $_SERVER['SCRIPT_FILENAME']!==__FILE__;
+if($debug){
+  ob_clean();
+}
+function ok(stdClass $cls, Array $arr=[]):string{
+  return 'haha';
+}
+
+function xxx($arr){ // {{{
+  foreach($arr as $k=>$v){
+    $t = gettype($v);
+    switch($t){
+    case 'integer':
+    case 'double':
+      echo "<li><dfn>$k</dfn> = <var class=$t>$v</var></li>";
+      break;
+    case 'boolean':
+      $v = $v?'true':'false';
+      echo "<li><dfn>$k</dfn> = <var class=$t>$v</var></li>";
+      break;
+    case 'NULL':
+      echo "<li><dfn>$k</dfn> = <var class=$t>null</var></li>";
+      break;
+    case 'string':
+      $v=str_replace(["\r","\n"],['\r','\n'],$v);
+      echo "<li><dfn>$k</dfn> = \"<var class=$t>$v</var>\"</li>";
+      break;
+    case 'object':
+      $cls = get_class($v);
+      switch($cls){
+      case 'ReflectionFunction':
+        echo '<li>';
+        echo '<a href=//php.net/',str_replace('_','-',$v->name),"><dfn>{$v->name}</dfn></a>";
+        echo ' ( <span class=args>';
+        foreach($v->getParameters() as $key=>$arg){
+          $c = [','][!$key];
+          $n = ['&'][!$key].'$'.$arg->getName();
+          $t = $arg->getType();
+          if($t) $t = "<a href=//php.net/$t class=type>$t</a> ";
+          echo $arg->isOptional()?"<span class=optional> [$c <dfn>$t</dfn> <var>$n</var> ":"<strong class=required>$c <dfn>$t</dfn> <var>$n</var></strong>";
+        }
+        echo str_repeat(']</span>',$v->getNumberOfParameters()-$v->getNumberOfRequiredParameters());
+        echo '</span> )';
+
+        $r = $v->getReturnType();
+        if($r) echo " :<span class=syn>$r</span>";
+        echo ' <small>{ ... }</small>';
+        $f = $v->getFileName();
+        //FIXME: How to open local file
+        if($f) echo " <a href=file://$f>$f</a> ", $v->getStartLine();
+
+        if($v->isDeprecated()) echo ' <strong>DEPRECATED</strong>';
+
+
+        if(in_array($v->name,[
+  'zend_version',
+  'time',
+  'timezone_version_get',
+  'date_default_timezone_get',
+  'mhash_count',
+  'session_name',
+  'session_module_name',
+  'session_save_path',
+  'phpversion',
+  'php_sapi_name',
+  'php_uname',
+  'php_ini_scanned_files',
+  'php_ini_loaded_file',
+  'rand',
+  'getrandmax',
+  'mt_getrandmax',
+  'getmyuid',
+  'getmygid',
+  'getmypid',
+  'getmyinode',
+  'getlastmod',
+  'pi',
+  'microtime',
+  'uniqid',
+  'get_current_user',
+  'memory_get_usage',
+  'memory_get_peak_usage',
+  'get_include_path',
+  'gethostname',
+  'umask',
+  'getcwd',
+  'realpath_cache_size',
+  'lcg_value',
+  'sys_get_temp_dir',
+  'easter_date',
+  'easter_days',
+  'unixtojd',
+  'imagetypes',
+  'json_last_error_msg',
+])){
+          $tmp1 = ($v->name)();
+          $tmp2 = gettype($tmp1);
+
+          echo ' ',is_string($tmp1)?"<output>\"$tmp1\"</output>":"<output>$tmp1</output>";
+
+        }
+        break;
+      case 'ReflectionClass':
+        echo "<li class=$t>";
+        echo '<span class=syn>',join(Reflection::getModifierNames($v->getModifiers()),' '), ' ',$v->isInterface()?'interface':($v->isTrait()?'trait':'class'),'</span> ';
+        echo '<a href=//php.net/class.',str_replace('\\','_',$v->name),"><dfn>{$v->name}</dfn></a>";
+
+        $p = $v->getParentClass();
+        if($p){
+          echo ' <span class=syn>extends</span> <a href=//php.net/class.',str_replace('\\','_',$p->name),'>',$p->name,'</a>';
+        }
+
+        $i = $v->getInterfaceNames();
+        if($i){
+          echo ' <span class=syn>implements</span> ';
+          echo join(array_map(function($v){return '<a href=//php.net/class.'.str_replace('\\','_',$v).'>'.$v.'</a>';}, $i),', ');
+        }
+        echo ' <small>{ ... }</small>';
+        break;
+      default:
+        echo "<li class=$t><dfn>{$v->name}</dfn>{} $cls</li>";
+      }
+      break;
+    default:
+      echo "<li class=\"$t\"><dfn>{$v->name}</dfn>$v xxxxxxxxxxxxxxxxx</li>";
+    }
+
+  }
+} // }}}
+?>
 <!doctype html>
 <meta charset=utf-8>
 <title><?=PHP_SAPI,' ',PHP_VERSION?></title>
@@ -111,126 +242,38 @@ h2 {
 }
 </style>
 
-<?php
-
-function xxx($arr){ // {{{
-  foreach($arr as $k=>$v){
-    $t = gettype($v);
-    switch($t){
-    case 'integer':
-    case 'double':
-      echo "<li><dfn>$k</dfn> = <var class=$t>$v</var></li>";
-      break;
-    case 'boolean':
-      $v = $v?'true':'false';
-      echo "<li><dfn>$k</dfn> = <var class=$t>$v</var></li>";
-      break;
-    case 'NULL':
-      echo "<li><dfn>$k</dfn> = <var class=$t>null</var></li>";
-      break;
-    case 'string':
-      $v=str_replace(["\r","\n"],['\r','\n'],$v);
-      echo "<li><dfn>$k</dfn> = \"<var class=$t>$v</var>\"</li>";
-      break;
-    case 'object':
-      $cls = get_class($v);
-      switch($cls){
-      case 'ReflectionFunction':
-        echo '<li>';
-        echo '<a href=//php.net/',str_replace('_','-',$k),"><dfn>$k</dfn></a>";
-        echo ' ( <span class=args>';
-        foreach($v->getParameters() as $key=>$arg){
-          $c = [','][!$key];
-          $n = ['&'][!$key].'$'.$arg->getName();
-          $t = $arg->getType();
-          if($t) $t = "<a href=//php.net/$t class=type>$t</a> ";
-          echo $arg->isOptional()?"<span class=optional> [$c <dfn>$t</dfn> <var>$n</var> ":"<strong class=required>$c <dfn>$t</dfn> <var>$n</var></strong>";
-        }
-        echo str_repeat(']</span>',$v->getNumberOfParameters()-$v->getNumberOfRequiredParameters());
-        echo '</span> )';
-
-        if($v->isDeprecated()) echo ' <strong>DEPRECATED</strong>';
-
-        if(in_array($k,[
-  'zend_version',
-  'time',
-  'timezone_version_get',
-  'date_default_timezone_get',
-  'mhash_count',
-  'session_name',
-  'session_module_name',
-  'session_save_path',
-  'phpversion',
-  'php_sapi_name',
-  'php_uname',
-  'php_ini_scanned_files',
-  'php_ini_loaded_file',
-  'rand',
-  'getrandmax',
-  'mt_getrandmax',
-  'getmyuid',
-  'getmygid',
-  'getmypid',
-  'getmyinode',
-  'getlastmod',
-  'pi',
-  'microtime',
-  'uniqid',
-  'get_current_user',
-  'memory_get_usage',
-  'memory_get_peak_usage',
-  'get_include_path',
-  'gethostname',
-  'umask',
-  'getcwd',
-  'realpath_cache_size',
-  'lcg_value',
-  'sys_get_temp_dir',
-  'easter_date',
-  'easter_days',
-  'unixtojd',
-  'imagetypes',
-  'json_last_error_msg',
-])){
-          $tmp1 = $k();
-          $tmp2 = gettype($tmp1);
-
-          echo ' ',is_string($tmp1)?"<output>\"$tmp1\"</output>":"<output>$tmp1</output>";
-        }
-        break;
-      case 'ReflectionClass':
-        echo "<li class=$t>";
-        echo join(Reflection::getModifierNames($v->getModifiers()),' '), ' <span class=syn>class</span> ';
-        echo '<a href=//php.net/class.',$k,"><dfn>$k</dfn></a>";
-
-        $p = $v->getParentClass();
-        if($p){
-          echo " <span class=syn>extends</span> <a href=//php.net/class.{$p->name}>{$p->name}</a>";
-        }
-
-        $i = $v->getInterfaceNames();
-        if($i){
-          echo ' <span class=syn>implements</span> ';
-          echo join(array_map(function($v){return "<a href=//php.net/class.$v>$v</a>";}, $i),', ');
-        }
-        echo ' <small>{ ... }</small>';
-        break;
-      default:
-        echo "<li class=$t><dfn>$k</dfn>{} $cls</li>";
-      }
-      break;
-    default:
-      echo "<li class=\"$t\"><dfn>$k</dfn>$v xxxxxxxxxxxxxxxxx</li>";
-    }
-
-  }
-} // }}}
-?>
-
 
 <header>
 <h1><?=PHP_SAPI,' ',PHP_VERSION?></h1>
 </header>
+
+<?php
+if($debug){
+
+  echo '<p>', __FILE__;
+
+  echo '<h3>User Defined Constants</h3>';
+  echo '<ol>';
+  echo xxx(get_defined_constants(1)['user']);
+  echo '</ol>';
+
+  echo '<h3>User Defined Functions</h3>';
+  echo '<ol>';
+  echo xxx(array_map(function($v){return new ReflectionFunction($v);}, get_defined_functions()['user']));
+  echo '</ol>';
+
+  echo '<h3>User Declared Classes</h3>';
+  echo '<ol>';
+  echo xxx(array_filter(array_map(function($v){$c = new ReflectionClass($v);if($c->isUserDefined())return $c;}, get_declared_classes())));
+  echo '</ol>';
+
+  echo '<h3>User Declared Interfaces</h3>';
+  echo '<ol>';
+  echo xxx(array_filter(array_map(function($v){$c = new ReflectionClass($v);if($c->isUserDefined())return $c;}, get_declared_interfaces())));
+  echo '</ol>';
+  die();
+}
+?>
 
 <main>
 
@@ -292,5 +335,4 @@ foreach(get_loaded_extensions() as $ext){
   echo '</section>';
 
 }
-
 // vim: se fdm=marker:
